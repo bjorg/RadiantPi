b8f322693e7d8b6aa6de52cf075fba4d9a03cbd1
Steve Bjorg <steve.bjorg@gmail.com>
2021-01-17 09:00:39 -0800

    added CLI

---
+++ Src/RadiantPi.Cli/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * RadiantPi.Cli - Command line tool for Lumagen RadiancePro
+ * Copyright (C) 2020 - Steve G. Bjorg
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.IO.Ports;
+
+Console.WriteLine("RadiantPi CLI");
+Console.WriteLine();
+
+// list available serial ports
+Console.WriteLine("Available ports");
+foreach(var port in SerialPort.GetPortNames()) {
+    Console.WriteLine($"  {port}");
+}
