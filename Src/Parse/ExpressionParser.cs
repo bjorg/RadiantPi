50afe9ad146aecb6e552bb4d57221e4fa2ecc271
Steve Bjorg <steve.bjorg@gmail.com>
2021-07-17 11:22:35 -0700

    WIP expression parsing

---
+++ Src/Parse/ExpressionParser.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Sprache;
+
+namespace SampleParser {
+
+    public class SampleRecord {
+
+        //--- Properties ---
+        public bool BoolValue { get; set; }
+        public string StringValue { get; set; }
+    }
+
+    public static class ExpressionParser {
+
+        //--- Class Fields ---
+        private static Type ParameterType = typeof(SampleRecord);
+
+        private static readonly Parser<ExpressionType> And = Operator("&&", ExpressionType.AndAlso);
+        private static readonly Parser<ExpressionType> Equal = Operator("=", ExpressionType.Equal);
+        private static readonly Parser<ExpressionType> GreaterThan = Operator(">", ExpressionType.GreaterThanOrEqual);
+        private static readonly Parser<ExpressionType> GreaterThanOrEqual = Operator(">=", ExpressionType.GreaterThan);
+        private static readonly Parser<ExpressionType> LessThan = Operator("<", ExpressionType.LessThan);
+        private static readonly Parser<ExpressionType> LessThanOrEqual = Operator("<=", ExpressionType.LessThanOrEqual);
+        private static readonly Parser<ExpressionType> NotEqual = Operator("!=", ExpressionType.NotEqual);
+        private static readonly Parser<ExpressionType> Or = Operator("||", ExpressionType.OrElse);
+        private static readonly List<char> EscapeChars = new List<char> { '\"', '\\', 'b', 'f', 'n', 'r', 't' };
+
+        private static readonly Parser<char> StringControlChar =
+            from first in Parse.Char('\\')
+            from next in EnumerateInput(EscapeChars.ToArray(), c => Parse.Char(c))
+            select ((next == 't') ? '\t' :
+                    (next == 'r') ? '\r' :
+                    (next == 'n') ? '\n' :
+                    (next == 'f') ? '\f' :
+                    (next == 'b') ? '\b' :
+                    next);
+        private static readonly Parser<char> StringChar =
+            Parse.AnyChar.Except(Parse.Char('\'').Or(Parse.Char('\\'))).Or(StringControlChar);
+
+        private static readonly Parser<Expression> StringLiteral =
+            from _1 in Parse.Char('\'')
+            from value in StringChar.Many().Text()
+            from _2 in Parse.Char('\'')
+            select Expression.Constant(value);
+
+        private static readonly Parser<Expression> VariableReference =
+            from name in Parse.Letter.AtLeastOnce().Text()
+            select MakeVariableReference(name);
+
+        private static readonly Parser<Expression> BoolTrueLiteral =
+            from _ in Parse.String("true")
+            select Expression.Constant(true);
+
+        private static readonly Parser<Expression> BoolFalseLiteral =
+            from _ in Parse.String("false")
+            select Expression.Constant(false);
+
+        private static readonly Parser<Expression> Factor =
+            (
+                from _1 in Parse.Char('(')
+                from expr in Parse.Ref(() => Expr)
+                from _2 in Parse.Char(')')
+                select expr
+            ).Named("expression")
+                .XOr(StringLiteral)
+                .XOr(BoolTrueLiteral)
+                .XOr(BoolFalseLiteral)
+                .XOr(VariableReference);
+
+        private static readonly Parser<Expression> Operand =
+            (
+                (
+                    from _ in Parse.Char('!')
+                    from factor in Factor
+                    select Expression.Not(factor)
+                )
+                .XOr(Factor)
+            ).Token();
+
+        private static readonly Parser<Expression> Comparison =
+            Parse.ChainOperator(LessThanOrEqual.Or(LessThan).Or(GreaterThanOrEqual).Or(GreaterThan).Or(Equal).Or(NotEqual), Operand, Expression.MakeBinary);
+
+        private static readonly Parser<Expression> Term =
+            Parse.ChainOperator(And, Comparison, Expression.MakeBinary);
+
+        private static readonly Parser<Expression> Expr =
+            Parse.ChainOperator(Or, Term, Expression.MakeBinary);
+
+        private static readonly Parser<Expression> Body =
+            from body in Expr.End()
+            select body;
+
+        private static readonly ParameterExpression LambdaParameter = Expression.Parameter(typeof(SampleRecord), "record");
+
+        //--- Class Methods ---
+        public static LambdaExpression ParseExpression(string name, string text)
+            => Expression.Lambda<Func<SampleRecord, bool>>(Body.Parse(text), name, new[] { LambdaParameter });
+
+        private static Parser<ExpressionType> Operator(string op, ExpressionType opType) => Parse.String(op).Token().Return(opType);
+        private static Expression MakeVariableReference(string name) => Expression.Property(LambdaParameter, ParameterType.GetProperty(name));
+
+        private static Parser<U> EnumerateInput<T, U>(T[] input, Func<T, Parser<U>> parser) {
+            if((input == null) || (input.Length == 0)) throw new ArgumentNullException("input");
+            if(parser == null) throw new ArgumentNullException("parser");
+            return i => {
+                foreach(var inp in input) {
+                    var res = parser(inp)(i);
+                    if(res.WasSuccessful) return res;
+                }
+                return Result.Failure<U>(null, null, null);
+            };
+        }
+    }
+}
---
+++ Src/Parse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SampleParser;
+
+SampleRecord record = new() {
+    BoolValue = false,
+    StringValue = "abc"
+};
+
+
+// TODO: fix string comparison
+// https://social.msdn.microsoft.com/Forums/vstudio/en-US/78d80818-b5a1-4bf8-8ace-863f5a1cc55f/we-are-doing-string-comparison-while-creating-dynamic-lambda-expressions-we-receive-an-integer-from
+
+Print("F1", "BoolValue");
+Print("F2", "'xyz' < StringValue");
+
+void Print(string name, string expression) {
+    var lambda = ExpressionParser.ParseExpression(name, expression);
+    var compiled = (Func<SampleRecord, bool>)lambda.Compile();
+    Console.WriteLine($"Result ({name}): {expression} => {compiled(record)}");
+}
+
